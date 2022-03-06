4f61ca2c6bdceb3beaac8b17f6b6015c2702284e
brytebee <brytebee@gmail.com>
2022-03-06 23:53:22 +0100

    :white_check_mark: Make Profile pix a link
    Made profile pix and all social media handle as links to their respective pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,33 @@ import { FaMedium } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
+const handleTwitter = () => {
+  window.open("http://twitter.com/brytebee");
+};
+
+const handleGithub = () => {
+  window.open("https://github.com/brytebee");
+};
+
+const handleMedium = () => {
+  window.open("https://medium.com/@brytebee");
+};
+
+const handleLinkedIn = () => {
+  window.open("https://linkedin.com/in/brytebee");
+};
+
 const Header = () => (
   <>
     <div className="navigate d-flex gen-padding ">
-      <img
-        className="profile-pix"
-        style={{ width: "30px", height: "30px", borderRadius: "100%" }}
-        src={img}
-        alt="Atsighi Bright"
-      />
+      <NavLink to="/">
+        <img
+          className="profile-pix"
+          style={{ width: "30px", height: "30px", borderRadius: "100%" }}
+          src={img}
+          alt="Atsighi Bright"
+        />
+      </NavLink>
       <nav className="navigations">
         <ul className="d-flex">
           <li>
@@ -40,10 +58,10 @@ const Header = () => (
         </ul>
       </nav>
       <div className="handles">
-        <FaLinkedin className="handle" />
-        <FaGithub className="handle" />
-        <FaMedium className="handle" />
-        <FaTwitter className="handle" />
+        <a onClick={handleLinkedIn}>{<FaLinkedin className="handle" />}</a>
+        <a onClick={handleGithub}>{<FaGithub className="handle" />}</a>
+        <a onClick={handleMedium}>{<FaMedium className="handle" />}</a>
+        <a onClick={handleTwitter}>{<FaTwitter className="handle" />}</a>
       </div>
     </div>
     <hr />
