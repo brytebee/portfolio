40c93d83e9236a33b05d90791a2c22e447cf81bb
brytebee <brytebee@gmail.com>
2022-03-16 12:47:49 +0100

    :lipstick: Update project cards styles
    Updated with on desktop and added hover styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Projects = () => (
-  <div className="gen-padding projects">
-    <div className="top">
-      <div className="card">React</div>
-      <div className="card">Rails</div>
-    </div>
-    <div className="bottom">
-      <div className="card">JavaScript</div>
-      <div className="card">Landing pages</div>
+  <div className="gen-padding">
+    <div className="projects">
+      <div className="top">
+        <div className="card">React</div>
+        <div className="card">Rails</div>
+      </div>
+      <div className="bottom">
+        <div className="card">JavaScript</div>
+        <div className="card">Landing pages</div>
+      </div>
     </div>
   </div>
 );
