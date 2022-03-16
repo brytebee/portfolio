79d2c6f2fa16338fa30aa0204f22069219bef0d0
brytebee <brytebee@gmail.com>
2022-03-16 14:12:04 +0100

    :white_check_mark: Add Images
    Added images of react projects

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const ReactPages = () => <div className="gen-padding">ReactPages</div>;
+const ReactPages = () => (
+  <div className="gen-padding">
+    <div className="list">
+      <img
+        className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+        alt="Covid19 Stats"
+      />
+      <img
+        className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+        alt="Books store"
+      />
+    </div>
+  </div>
+);
 
 export default ReactPages;
