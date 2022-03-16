245c382f4bbc671c26c7f7c6760b70995e624301
brytebee <brytebee@gmail.com>
2022-03-16 16:18:02 +0100

    :lipstick: Style gallery
    Style gallery to display flex

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -4,12 +4,13 @@ const ReactPages = () => {
   const [show, setShow] = useState(1);
 
   const handleClick = (e) => {
-    if (e.target.id === 1) {
+    if (e.target.id === 2) {
+      setShow(2);
     }
   };
 
   return (
-    <div className="gen-padding">
+    <div className="gen-padding gallery">
       <div className="list">
         <img
           className="list-image"
@@ -47,20 +48,26 @@ const ReactPages = () => {
         />
       </div>
       <div className="images">
-        <img
-          className="show-image hidden"
-          onClick={handleClick}
-          id="image1"
-          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-          alt="Covid19 Stats"
-        />
-        <img
-          className="show-image hidden"
-          onClick={handleClick}
-          id="image2"
-          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-          alt="Books store"
-        />
+        {show === 1 ? (
+          <img
+            className="show-image hidden"
+            id="image1"
+            src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+            alt="Covid19 Stats"
+          />
+        ) : (
+          ''
+        )}
+        {show === 2 ? (
+          <img
+            className="show-image hidden"
+            id="image2"
+            src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+            alt="Books store"
+          />
+        ) : (
+          ''
+        )}
       </div>
     </div>
   );
