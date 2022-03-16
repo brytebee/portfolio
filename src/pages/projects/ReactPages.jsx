26b1ec21901d10689ae8a912c453ae65487963e9
brytebee <brytebee@gmail.com>
2022-03-16 15:29:52 +0100

    :white_check_mark: Add dispay column
    Added the column to display project

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+const handleClick = (e) => {
+  console.log('target', e.target.id);
+};
 
 const ReactPages = () => (
   <div className="gen-padding">
     <div className="list">
-      <Link to="">
-        <img
-          className="list-image"
-          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-          alt="Covid19 Stats"
-        />
-      </Link>
       <img
         className="list-image"
+        onClick={handleClick}
+        id="1"
+        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+        alt="Covid19 Stats"
+      />
+      <img
+        className="list-image"
+        onClick={handleClick}
+        id="2"
         src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
         alt="Books store"
       />
@@ -33,6 +38,22 @@ const ReactPages = () => (
       />
       <img
         className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+        alt="Books store"
+      />
+    </div>
+    <div className="images">
+      <img
+        className="show-image hidden"
+        onClick={handleClick}
+        id="image1"
+        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+        alt="Covid19 Stats"
+      />
+      <img
+        className="show-image hidden"
+        onClick={handleClick}
+        id="image2"
         src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
         alt="Books store"
       />
