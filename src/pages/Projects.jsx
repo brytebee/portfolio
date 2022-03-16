3a7e805eebd12cfcd2af68636b192088346799e3
brytebee <brytebee@gmail.com>
2022-03-16 11:47:40 +0100

    :white_check_mark: Add Project cards
    Added cards for projects.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Projects = () => <div className="gen-padding">Projects</div>;
+const Projects = () => (
+  <div className="gen-padding">
+    <div className="card">React</div>
+    <div className="card">Rails</div>
+    <div className="card">JS</div>
+    <div className="card">Landing pages</div>
+  </div>
+);
 
 export default Projects;
