119661b34da97582ef0930d0deb9200937fd3f1c
brytebee <brytebee@gmail.com>
2022-03-16 15:53:54 +0100

    :white_check_mark: Import useState
    Import useState hock to manage gallery display

--- src/pages/projects/JSPages.jsx
+++ src/pages/projects/JSPages.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const JSPages = () => <div className="gen-padding">JSPages</div>;
+const JSPages = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="gen-padding">
+      <button onClick={() => setShow((prev) => !prev)}>Click</button>
+      {show && <p>This is your component</p>}
+    </div>
+  );
+};
 
 export default JSPages;
--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -1,64 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const handleClick = (e) => {
   console.log('target', e.target.id);
+  if (e.target.id === 1) {
+  }
 };
 
-const ReactPages = () => (
-  <div className="gen-padding">
-    <div className="list">
-      <img
-        className="list-image"
-        onClick={handleClick}
-        id="1"
-        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-        alt="Covid19 Stats"
-      />
-      <img
-        className="list-image"
-        onClick={handleClick}
-        id="2"
-        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-        alt="Books store"
-      />
-      <img
-        className="list-image"
-        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-        alt="Covid19 Stats"
-      />
-      <img
-        className="list-image"
-        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-        alt="Books store"
-      />
-      <img
-        className="list-image"
-        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-        alt="Covid19 Stats"
-      />
-      <img
-        className="list-image"
-        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-        alt="Books store"
-      />
+const ReactPages = () => {
+  const [show, setShow] = useState(1);
+  return (
+    <div className="gen-padding">
+      <div className="list">
+        <img
+          className="list-image"
+          onClick={handleClick}
+          id="1"
+          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+          alt="Covid19 Stats"
+        />
+        <img
+          className="list-image"
+          onClick={handleClick}
+          id="2"
+          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+          alt="Books store"
+        />
+        <img
+          className="list-image"
+          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+          alt="Covid19 Stats"
+        />
+        <img
+          className="list-image"
+          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+          alt="Books store"
+        />
+        <img
+          className="list-image"
+          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+          alt="Covid19 Stats"
+        />
+        <img
+          className="list-image"
+          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+          alt="Books store"
+        />
+      </div>
+      <div className="images">
+        <img
+          className="show-image hidden"
+          onClick={handleClick}
+          id="image1"
+          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+          alt="Covid19 Stats"
+        />
+        <img
+          className="show-image hidden"
+          onClick={handleClick}
+          id="image2"
+          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+          alt="Books store"
+        />
+      </div>
     </div>
-    <div className="images">
-      <img
-        className="show-image hidden"
-        onClick={handleClick}
-        id="image1"
-        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-        alt="Covid19 Stats"
-      />
-      <img
-        className="show-image hidden"
-        onClick={handleClick}
-        id="image2"
-        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-        alt="Books store"
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default ReactPages;
