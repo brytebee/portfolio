4e62e0b5ea67ac6cb6ac352ff2a85f999f7d207c
brytebee <brytebee@gmail.com>
2022-03-16 12:22:22 +0100

    :lipstick: Style project cards
    Added styles to project styles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Projects = () => (
-  <div className="gen-padding">
-    <div className="card">React</div>
-    <div className="card">Rails</div>
-    <div className="card">JS</div>
-    <div className="card">Landing pages</div>
+  <div className="gen-padding projects">
+    <div className="top">
+      <div className="card">React</div>
+      <div className="card">Rails</div>
+    </div>
+    <div className="bottom">
+      <div className="card">JavaScript</div>
+      <div className="card">Landing pages</div>
+    </div>
   </div>
 );
 
