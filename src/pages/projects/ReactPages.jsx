dca98e0372c4cb22563423a4afd45007b8aae5d9
brytebee <brytebee@gmail.com>
2022-03-16 14:40:42 +0100

    :white_check_mark: Add stack gallery
    Added and styled stack gallery

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ReactPages = () => (
   <div className="gen-padding">
     <div className="list">
+      <Link to="">
+        <img
+          className="list-image"
+          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+          alt="Covid19 Stats"
+        />
+      </Link>
+      <img
+        className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+        alt="Books store"
+      />
+      <img
+        className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
+        alt="Covid19 Stats"
+      />
+      <img
+        className="list-image"
+        src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+        alt="Books store"
+      />
       <img
         className="list-image"
         src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
