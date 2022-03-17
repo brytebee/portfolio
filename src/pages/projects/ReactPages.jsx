fe88a9bfde40700d960d11116e58962e4e9db472
brytebee <brytebee@gmail.com>
2022-03-17 12:16:44 +0100

    :white_check_mark: Add switch logic
    Add the logic to switch between the selected project

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -4,9 +4,8 @@ const ReactPages = () => {
   const [show, setShow] = useState(1);
 
   const handleClick = (e) => {
-    if (e.target.id === 2) {
-      setShow(2);
-    }
+    console.log(e.target.id);
+    setShow(Number(e.target.id));
   };
 
   return (
@@ -26,31 +25,11 @@ const ReactPages = () => {
           src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
           alt="Books store"
         />
-        <img
-          className="list-image"
-          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-          alt="Covid19 Stats"
-        />
-        <img
-          className="list-image"
-          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-          alt="Books store"
-        />
-        <img
-          className="list-image"
-          src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
-          alt="Covid19 Stats"
-        />
-        <img
-          className="list-image"
-          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
-          alt="Books store"
-        />
       </div>
       <div className="images">
         {show === 1 ? (
           <img
-            className="show-image hidden"
+            className="show-image"
             id="image1"
             src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
             alt="Covid19 Stats"
@@ -60,7 +39,7 @@ const ReactPages = () => {
         )}
         {show === 2 ? (
           <img
-            className="show-image hidden"
+            className="show-image"
             id="image2"
             src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
             alt="Books store"
@@ -70,22 +49,48 @@ const ReactPages = () => {
         )}
       </div>
       <div className="description">
-        <div id="discription1" className="desc">
-          <h3>React | COVID19 STATS</h3>
-          <p>
-            A covid19 stats app build with ReactJS at Microverse. Built with
-            React, Redux and API. I wrote tests using Jest and React Testing
-            Library.
-          </p>
-          <ul>
-            <li>ReactJS - Function based component</li>
-            <li>Redux</li>
-            <li>API</li>
-          </ul>
-          <a href="https://brytebee.github.io/Bookstore-React/" target="_blank">
-            Live link
-          </a>
-        </div>
+        {show === 1 && (
+          <div id="discription1" className="desc">
+            <h3>React | COVID19 STATS</h3>
+            <p>
+              A covid19 stats app build with ReactJS at Microverse. Built with
+              React, Redux and API. I wrote tests using Jest and React Testing
+              Library.
+            </p>
+            <ul>
+              <li>ReactJS - Function based component</li>
+              <li>Redux</li>
+              <li>API</li>
+            </ul>
+            <a
+              href="http://capstone-react-brytebee.herokuapp.com/"
+              target="_blank"
+            >
+              Covid19 Live link
+            </a>
+          </div>
+        )}
+        {show === 2 && (
+          <div id="discription2" className="desc">
+            <h3>Bookstore-React</h3>
+            <p>
+              This project is used to display a list of books. Users can
+              add/remove books from the list. It is built with ReactJS and
+              Redux. It implements Routing, and Components concepts
+            </p>
+            <ul>
+              <li>ReactJS - Function based component</li>
+              <li>Redux</li>
+              <li>API</li>
+            </ul>
+            <a
+              href="https://brytebee.github.io/Bookstore-React/"
+              target="_blank"
+            >
+              Bookstore Live link
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
