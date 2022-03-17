a6ebfd9760398146c6ed54062fe62c05b7dff790
brytebee <brytebee@gmail.com>
2022-03-17 15:32:42 +0100

    :white_check_mark: Add into paragraph
    Added an intro paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react';
 
-const About = () => <div className="gen-padding">About</div>;
+const About = () => (
+  <div className="gen-padding">
+    <h2>Hello there!</h2>
+    <p>
+      I used to be a sales representative but that's a fancy term. In my
+      country, Nigeria, it is called sales boy. I worked in this position for
+      about 3 years while waiting for a dream job.
+    </p>
+    <img
+      style={{ width: '700px' }}
+      src="https://access1.imglarger.com/results/NAOYR73v_male_3_cartoon11.png"
+      alt="Bright cartooned playing guitar"
+    />
+  </div>
+);
 
 export default About;
