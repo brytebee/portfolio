7a6e4cf60b6e8255c82dbc1abe4c2607385a998f
brytebee <brytebee@gmail.com>
2022-03-17 16:03:59 +0100

    :white_check_mark: More structure
    Placed paragraphs and image in a section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,46 +2,48 @@ import React from 'react';
 
 const About = () => (
   <div className="gen-padding">
-    <div>
-      <h2>Hello there!</h2>
-      <p>
-        I used to be a sales representative but that's a fancy term. In my
-        country, Nigeria, it is called sales boy. I worked in this position for
-        about 3 years while waiting for a dream job.
-      </p>
-      <p>
-        A dream job refers to a white/blue-collar job in my locality. And
-        although I had heard and loved programming earlier, I never knew how to
-        become one.
-      </p>
-      <p>
-        Then came Andela 4.0 in 2019. This was a programme by google, Andela and
-        Pluralsight, a fourth in its series and an entry for me into the tech
-        space.
-      </p>
-      <p>
-        During the pandemic, I quit my job and began a full-time journey with
-        the help of family and mentors. I took courses from freeCodeCamp,
-        Coursera, Udacity and Udemy and as well YouTube.
-      </p>
-      <p>
-        Soon I learned about Microverse and the beautiful opportunity it offered
-        programmers. I joined microverse in January 2021 and as the saying goes,
-        the rest is history…
-      </p>
-      <p>
-        Today, I have learned some of the best technologies in the industry. I
-        build and learn at the same time. This is why you should consider me to
-        be a part of your team or build your solution.
-      </p>
-      <p>Let's build better solutions and a better world.</p>
-      <h6>Atsighi Bright</h6>
-    </div>
-    <img
-      style={{ width: '700px' }}
-      src="https://access1.imglarger.com/results/NAOYR73v_male_3_cartoon11.png"
-      alt="Bright cartooned playing guitar"
-    />
+    <section className="about-content">
+      <div>
+        <h2>Hello there!</h2>
+        <p>
+          I used to be a sales representative but that's a fancy term. In my
+          country, Nigeria, it is called sales boy. I worked in this position
+          for about 3 years while waiting for a dream job.
+        </p>
+        <p>
+          A dream job refers to a white/blue-collar job in my locality. And
+          although I had heard and loved programming earlier, I never knew how
+          to become one.
+        </p>
+        <p>
+          Then came Andela 4.0 in 2019. This was a programme by google, Andela
+          and Pluralsight, a fourth in its series and an entry for me into the
+          tech space.
+        </p>
+        <p>
+          During the pandemic, I quit my job and began a full-time journey with
+          the help of family and mentors. I took courses from freeCodeCamp,
+          Coursera, Udacity and Udemy and as well YouTube.
+        </p>
+        <p>
+          Soon I learned about Microverse and the beautiful opportunity it
+          offered programmers. I joined microverse in January 2021 and as the
+          saying goes, the rest is history…
+        </p>
+        <p>
+          Today, I have learned some of the best technologies in the industry. I
+          build and learn at the same time. This is why you should consider me
+          to be a part of your team or build your solution.
+        </p>
+        <p>Let's build better solutions and a better world.</p>
+        <h3>Atsighi Bright</h3>
+      </div>
+      <img
+        style={{ width: '700px' }}
+        src="https://access1.imglarger.com/results/NAOYR73v_male_3_cartoon11.png"
+        alt="Bright cartooned playing guitar"
+      />
+    </section>
   </div>
 );
 
