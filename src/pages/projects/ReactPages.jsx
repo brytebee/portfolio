7ef92c71f54b06c2aac53d6ee3a61120b7df9e82
brytebee <brytebee@gmail.com>
2022-03-17 12:33:34 +0100

    :white_check_mark: Add Todo list
    Added the todo list project

--- src/pages/projects/ReactPages.jsx
+++ src/pages/projects/ReactPages.jsx
@@ -25,27 +25,45 @@ const ReactPages = () => {
           src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
           alt="Books store"
         />
+        <img
+          className="list-image"
+          onClick={handleClick}
+          id="3"
+          src="https://user-images.githubusercontent.com/27709832/154051255-6d06148d-d50a-447e-89e4-32eb3cf8b06c.png"
+          alt="Modern Todo List"
+        />
+        <img
+          className="list-image"
+          onClick={handleClick}
+          id="2"
+          src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
+          alt="Books store"
+        />
       </div>
       <div className="images">
-        {show === 1 ? (
+        {show === 1 && (
           <img
             className="show-image"
             id="image1"
             src="https://user-images.githubusercontent.com/27709832/157883026-89e3ecca-f26d-46c6-9917-2dc1dc072316.png"
             alt="Covid19 Stats"
           />
-        ) : (
-          ''
         )}
-        {show === 2 ? (
+        {show === 2 && (
           <img
             className="show-image"
             id="image2"
             src="https://user-images.githubusercontent.com/27709832/158596891-26a5f175-b4e1-4963-82c5-9e12f4da263c.png"
             alt="Books store"
           />
-        ) : (
-          ''
+        )}
+        {show === 3 && (
+          <img
+            className="show-image"
+            id="image3"
+            src="https://user-images.githubusercontent.com/27709832/154051255-6d06148d-d50a-447e-89e4-32eb3cf8b06c.png"
+            alt="Books store"
+          />
         )}
       </div>
       <div className="description">
@@ -88,6 +106,23 @@ const ReactPages = () => {
               target="_blank"
             >
               Bookstore Live link
+            </a>
+          </div>
+        )}
+        {show === 3 && (
+          <div id="discription2" className="desc">
+            <h3>React TodoList App</h3>
+            <p>
+              A simple todo list. The application is built with ReactJS and
+              local storage.
+            </p>
+            <ul>
+              <li>ReactJS</li>
+              <li>DOM</li>
+              <li>Local storage</li>
+            </ul>
+            <a href="https://brytebee.github.io/react-todo/" target="_blank">
+              Modern Todo List Live link
             </a>
           </div>
         )}
