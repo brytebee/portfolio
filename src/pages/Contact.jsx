4af8d487fc233431645918dfba9f9e450bc00ff7
brytebee <brytebee@gmail.com>
2022-03-18 22:33:30 +0100

    :white_check_mark: Add Name input
    Added a name input box

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Contact = () => (
   <div className="gen-padding">
     <h2>Contact Me📧</h2>
-    <form action="" method="post"></form>
+    <form action="" method="post">
+      <input type="text" placeholder="Enter Name" />
+    </form>
   </div>
 );
 
