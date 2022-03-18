3b6018227cf01d6f0c5957118a1e97cab75648b5
brytebee <brytebee@gmail.com>
2022-03-18 23:41:10 +0100

    :lipstick: Update basic contact style
    Repositioned the contact form items on page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 const Contact = () => (
   <div className="gen-padding" style={{ textAlign: 'center' }}>
     <h2>Contact Me📧</h2>
-    <form
-      action=""
-      method="post"
-      style={{
-        width: '400px',
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <section>
+    <form action="" method="post">
+      <section
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
         <input type="text" placeholder="Name" required />
         <input type="email" placeholder="Email Address" required />
         <textarea
